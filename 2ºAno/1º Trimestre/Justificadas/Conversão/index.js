6adb36157b96785ade09d893cdbe25c3b372433b
Vini <vi058587@intelbras.com.br>
2023-06-06 17:10:53 -0300

    Atualizações de CSS

--- 2ºAno/1º Trimestre/Justificadas/Conversão/index.js
+++ 2ºAno/1º Trimestre/Justificadas/Conversão/index.js
@@ -1,6 +1,13 @@
+//início da função 
 function converte() {
+
+    //A variável temperatura é definida pelo valor digitado pelo usuário
     var temperatura = Number(document.getElementById("temperatura").value)
+
+    //A variável resultadoFinal é definida inicialmente como 0, para ter seu valor atribuído pelo cálculo escolhido
     var resultadoFinal = 0
+
+    //Escolha de cálculo e sua seguinte equação (checked é usado para verificar se o cálculo foi escolhido) 
     if (document.getElementById("cf").checked) {
         resultadoFinal = temperatura * (9 / 5) + 32
     }
@@ -20,7 +27,6 @@ function converte() {
         resultadoFinal = (temperatura - 273.15) * (9 / 5) + 32
     }
 
+    //Com o resultado, ele o atribui para a página HTML
     document.getElementById("resultado").innerHTML = resultadoFinal
 }
-
-converte()
