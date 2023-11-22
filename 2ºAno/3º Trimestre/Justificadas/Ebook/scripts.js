9fbfbefd12e6c46448e30cb403b34537b9e4e745
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2023-11-22 16:32:08 -0300

    Sidebar

--- 2ºAno/3º Trimestre/Justificadas/Ebook/scripts.js
+++ 2ºAno/3º Trimestre/Justificadas/Ebook/scripts.js
@@ -2,12 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const icon = document.getElementById('menu-icon');
     const open = document.getElementById('open-sidebar');
     const sidebar = document.querySelector('.sidebar');
+    const main = document.querySelector('main');
 
     open.addEventListener('click', function () {
         sidebar.classList.toggle('sidebar-hidden');
+        main.classList.toggle('main-right');
     });
-    
+
     icon.addEventListener('click', function () {
         sidebar.classList.toggle('sidebar-hidden');
+        main.classList.toggle('main-right');
     });
 });
