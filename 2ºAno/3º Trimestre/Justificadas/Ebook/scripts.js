2e98bced128904876ef1df9f33cca5de87a6ac11
vi058587 <vinicius.franca@intelbras.com>
2023-11-23 16:50:22 -0300

    Textos

--- 2ºAno/3º Trimestre/Justificadas/Ebook/scripts.js
+++ 2ºAno/3º Trimestre/Justificadas/Ebook/scripts.js
@@ -1,40 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const menu = function () {
-        sidebar.classList.toggle('sidebar-hidden');
-        main.classList.toggle('main-total');
-    };
-
     const icon = document.getElementById('menu-icon');
     const open = document.getElementById('open-sidebar');
     const sidebar = document.querySelector('.sidebar');
     const main = document.querySelector('main');
-    open.addEventListener('click', menu);
-    icon.addEventListener('click', menu);
-});
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Função para mostrar/ocultar seções com base no botão clicado
-    function showSection(sectionId) {
-        // Oculta todas as seções
-        const sections = document.querySelectorAll('.main-content section');
-        sections.forEach(section => {
-            section.style.display = 'none';
-        });
+    open.addEventListener('click', function () {
+        sidebar.classList.toggle('sidebar-hidden');
+        main.classList.toggle('main-right');
+    });
 
-        // Mostra a seção correspondente ao botão clicado
-        const selectedSection = document.getElementById(sectionId);
-        if (selectedSection) {
-            selectedSection.style.display = 'block';
-        }
-    }
-
-    // Adicionar ouvintes de eventos de clique a cada botão
-    for (let i = 1; i <= 4; i++) {
-        const button = document.getElementById(`capitulo${i}`);
-        if (button) {
-            button.addEventListener('click', function () {
-                showSection(`capitulo${i}`);
-            });
-        }
-    }
+    icon.addEventListener('click', function () {
+        sidebar.classList.toggle('sidebar-hidden');
+        main.classList.toggle('main-right');
+    });
 });
