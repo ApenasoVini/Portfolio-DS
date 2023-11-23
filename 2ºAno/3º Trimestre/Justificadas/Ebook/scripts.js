cb9816e7e62dfc97a2a8fd600f8df6b9e153a9a1
vi058587 <vinicius.franca@intelbras.com>
2023-11-23 16:34:35 -0300

    Textos

--- 2ºAno/3º Trimestre/Justificadas/Ebook/scripts.js
+++ 2ºAno/3º Trimestre/Justificadas/Ebook/scripts.js
@@ -11,3 +11,30 @@ document.addEventListener('DOMContentLoaded', function () {
     open.addEventListener('click', menu);
     icon.addEventListener('click', menu);
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Função para mostrar/ocultar seções com base no botão clicado
+    function showSection(sectionId) {
+        // Oculta todas as seções
+        const sections = document.querySelectorAll('.main-content section');
+        sections.forEach(section => {
+            section.style.display = 'none';
+        });
+
+        // Mostra a seção correspondente ao botão clicado
+        const selectedSection = document.getElementById(sectionId);
+        if (selectedSection) {
+            selectedSection.style.display = 'block';
+        }
+    }
+
+    // Adicionar ouvintes de eventos de clique a cada botão
+    for (let i = 1; i <= 4; i++) {
+        const button = document.getElementById(`capitulo${i}`);
+        if (button) {
+            button.addEventListener('click', function () {
+                showSection(`capitulo${i}`);
+            });
+        }
+    }
+});
