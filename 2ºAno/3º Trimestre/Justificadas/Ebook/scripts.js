5e0b133b2ab7126b0a1f6beaf8a055292246fe3c
vi058587 <vinicius.franca@intelbras.com>
2023-11-23 15:17:55 -0300

    Sidebar

--- 2ºAno/3º Trimestre/Justificadas/Ebook/scripts.js
+++ 2ºAno/3º Trimestre/Justificadas/Ebook/scripts.js
@@ -1,16 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const menu = function () {
+        sidebar.classList.toggle('sidebar-hidden');
+        main.classList.toggle('main-total');
+    };
+
     const icon = document.getElementById('menu-icon');
     const open = document.getElementById('open-sidebar');
     const sidebar = document.querySelector('.sidebar');
     const main = document.querySelector('main');
-
-    open.addEventListener('click', function () {
-        sidebar.classList.toggle('sidebar-hidden');
-        main.classList.toggle('main-total');
-    });
-
-    icon.addEventListener('click', function () {
-        sidebar.classList.toggle('sidebar-hidden');
-        main.classList.toggle('main-total');
-    });
+    open.addEventListener('click', menu);
+    icon.addEventListener('click', menu);
 });
