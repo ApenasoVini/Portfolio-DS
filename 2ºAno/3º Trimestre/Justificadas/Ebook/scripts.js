23c5db5bb9b97a929791273b295569936be129c3
vini <viniciusicmsc@gmail.com>
2023-11-24 08:42:40 -0300

    Sidebar

--- 2ºAno/3º Trimestre/Justificadas/Ebook/scripts.js
+++ 2ºAno/3º Trimestre/Justificadas/Ebook/scripts.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', function () {
     open.addEventListener('click', function () {
         sidebar.classList.toggle('sidebar-hidden');
         main.classList.toggle('main-total');
+        open.style.visibility  = 'hidden';
+        open.style.pointerEvents = 'none';
     });
 
     icon.addEventListener('click', function () {
