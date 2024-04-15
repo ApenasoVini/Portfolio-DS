541abc34bb6aa66eaf836ff480879d48a095e560
ApenasoVini <viniciusicmsc@gmail.com>
2024-04-15 12:01:10 -0300

    About

---
+++ assets/js/slides.js
@@ -0,0 +1,20 @@
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+showSlide(currentSlide);
+document.querySelector('.next').addEventListener('click', nextSlide);
+document.querySelector('.prev').addEventListener('click', prevSlide);
