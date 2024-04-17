ea376f59272c4d451180235bc7ab4cf9da65b36f
ApenasoVini <viniciusicmsc@gmail.com>
2024-04-16 22:11:14 -0300

    Atividades

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,7 +1,17 @@
-document.getElementById('download').addEventListener('click', function () {
+//Currículo
+document.getElementById('download-curriculo').addEventListener('click', function () {
     var a = document.createElement('a');
     a.href = '/assets/pdf/currículo.pdf';
     a.download = 'currículo.pdf';
+    a.click();
+    a.remove();
+});
+
+//Linguagens Fev
+document.getElementById('download-fev-ling').addEventListener('click', function () {
+    var a = document.createElement('a');
+    a.href = '/assets/pdf/linguagensFev.pdf';
+    a.download = 'linguagensFev.pdf';
     a.click();
     a.remove();
 });
