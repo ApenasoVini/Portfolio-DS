8db770778ff46936a8b29eb7cdd1fd9338ac350c
ApenasoVini <viniciusicmsc@gmail.com>
2024-04-17 18:55:33 -0300

    Atividades

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,5 +1,5 @@
 //Currículo
-function curriculo(){
+function curriculo() {
     var a = document.createElement('a');
     a.href = '/assets/pdf/currículo.pdf';
     a.download = 'currículo.pdf';
@@ -8,10 +8,28 @@ function curriculo(){
 }
 
 //Linguagens Fev
-function fevLing(){
+function fevLin() {
     var a = document.createElement('a');
     a.href = '/assets/pdf/linguagensFev.pdf';
     a.download = 'linguagensFev.pdf';
     a.click();
     a.remove();
+}
+
+//Matemática Fev
+function fevMat() {
+    window.open('https://www.youtube.com/watch?v=e-V4prJxsxY&ab_channel=Vin%C3%ADciusSoaresFran%C3%A7a', '_blank');
+}
+
+function fevHum() {
+    var a = document.createElement('a');
+    a.href = '/assets/pdf/humanasFev.pdf';
+    a.download = 'humanasFev.pdf';
+    a.click();
+    a.remove();
+}
+
+//Natureza Fev
+function fevNat() {
+    window.open('https://www.youtube.com/watch?v=eA0hSTFzL2A&ab_channel=BernardoLa', '_blank');
 }
