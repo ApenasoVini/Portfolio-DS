3bdd1d728b9a79d2aec0c81e1bbe95564479ee7a
ApenasoVini <viniciusicmsc@gmail.com>
2024-04-18 16:06:30 -0300

    Assets

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,7 +1,7 @@
 //Currículo
 function curriculo() {
     var a = document.createElement('a');
-    a.href = '../pdf/currículo.pdf';
+    a.href = '../../assets/pdf/currículo.pdf';
     a.download = 'currículo.pdf';
     a.click();
     a.remove();
@@ -10,7 +10,7 @@ function curriculo() {
 //Linguagens Fev
 function fevLin() {
     var a = document.createElement('a');
-    a.href = '../pdf/linguagensFev.pdf';
+    a.href = '../../assets/pdf/linguagensFev.pdf';
     a.download = 'linguagensFev.pdf';
     a.click();
     a.remove();
@@ -24,7 +24,7 @@ function fevMat() {
 //Humanas Fev
 function fevHum() {
     var a = document.createElement('a');
-    a.href = '../pdf/humanasFev.pdf';
+    a.href = '../../assets/pdf/humanasFev.pdf';
     a.download = 'humanasFev.pdf';
     a.click();
     a.remove();
