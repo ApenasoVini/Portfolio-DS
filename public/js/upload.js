d49e34d7d5f3536930f051c0a98276926a130179
ApenasoVini <viniciusicmsc@gmail.com>
2024-04-18 16:25:34 -0300

    Assets

--- public/js/upload.js
+++ public/js/upload.js
@@ -7,30 +7,30 @@ function curriculo() {
     a.remove();
 }
 
-//Linguagens Fev
-function fevLin() {
+//Linguagens 
+function lin1() {
     var a = document.createElement('a');
-    a.href = '../pdf/linguagensFev.pdf';
-    a.download = 'linguagensFev.pdf';
+    a.href = '../pdf/linguagens1.pdf';
+    a.download = 'linguagens1.pdf';
     a.click();
     a.remove();
 }
 
-//Matemática Fev
-function fevMat() {
+//Matemática 
+function mat() {
     window.open('https://www.youtube.com/watch?v=e-V4prJxsxY&ab_channel=Vin%C3%ADciusSoaresFran%C3%A7a', '_blank');
 }
 
-//Humanas Fev
-function fevHum() {
+//Humanas 
+function hum1() {
     var a = document.createElement('a');
-    a.href = '../pdf/humanasFev.pdf';
-    a.download = 'humanasFev.pdf';
+    a.href = '../pdf/humanas1.pdf';
+    a.download = 'humanas1.pdf';
     a.click();
     a.remove();
 }
 
-//Natureza Fev
-function fevNat() {
+//Natureza 
+function nat1() {
     window.open('https://www.youtube.com/watch?v=eA0hSTFzL2A&ab_channel=BernardoLa', '_blank');
 }
