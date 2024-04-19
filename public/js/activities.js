68590fc198f26014bfeec66cde4ed8d174bbf90d
ApenasoVini <viniciusicmsc@gmail.com>
2024-04-19 15:45:31 -0300

    Atividades

---
+++ public/js/activities.js
@@ -0,0 +1,9 @@
+//Matemática 
+function mat() {
+    window.open('https://www.youtube.com/watch?v=e-V4prJxsxY&ab_channel=Vin%C3%ADciusSoaresFran%C3%A7a', '_blank');
+}
+
+//Natureza 
+function nat1() {
+    window.open('https://www.youtube.com/watch?v=eA0hSTFzL2A&ab_channel=BernardoLa', '_blank');
+}
