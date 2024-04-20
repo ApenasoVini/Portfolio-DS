f1171cba8c474e4f97d52fad21284833b3fb0bc3
ApenasoVini <viniciusicmsc@gmail.com>
2024-04-20 15:26:54 -0300

    Atividades

--- public/js/activities.js
+++ public/js/activities.js
@@ -1,9 +1,39 @@
 //Matemática 
-function mat() {
+function mat1() {
     window.open('https://www.youtube.com/watch?v=e-V4prJxsxY&ab_channel=Vin%C3%ADciusSoaresFran%C3%A7a', '_blank');
+}
+
+function mat2() {
+    window.open('https://www.canva.com/design/DAF_H6nMATg/_wlNumsoJwtJJMWPqt-v3g/edit?utm_content=DAF_H6nMATg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton', '_blank');
+}
+
+//Linguagens 
+function lin1() {
+    window.open('https://docs.google.com/document/d/1AX11XX4vf7IBd_nFsxuQIhRSumjjw8capB7jof_k9_k/edit', '_blank');
+}
+
+function lin2() {
+    window.open('https://docs.google.com/document/d/17xZ2RV8aUe-kVnJldSHYjJ1f6PD4Rh9wzhCanlmtuyo/edit', '_blank');
 }
 
 //Natureza 
 function nat1() {
     window.open('https://www.youtube.com/watch?v=eA0hSTFzL2A&ab_channel=BernardoLa', '_blank');
+}
+
+function nat2() {
+    window.open('https://www.canva.com/design/DAGC-t1YLjQ/Llz5-X6nuUg0pJxPcd0E-A/edit?utm_content=DAGC-t1YLjQ&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton', '_blank');
+}
+
+//Natureza 
+function hum1() {
+    window.open('https://docs.google.com/document/d/18JDWzcYd91TVSL5abznQ4hpvMVgCjH_6gjImcE7HD5Q/edit?usp=sharing', '_blank');
+}
+
+function hum2() {
+    window.open('https://www.canva.com/design/DAF9n_vhkwE/zD8td3hlJLDviGBYWOxjmw/edit?utm_content=DAF9n_vhkwE&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton', '_blank');
+}
+
+function hum3() {
+    window.open('https://www.canva.com/design/DAF8TUtP4Aw/KNvPlzMdQY-xT4IQGtAoMQ/edit?utm_content=DAF8TUtP4Aw&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton', '_blank');
 }
