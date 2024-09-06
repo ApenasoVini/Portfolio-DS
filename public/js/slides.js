26bec5b219762031c34fb3f4e6ce5dcd28520576
vini <viniciusicmsc@gmail.com>
2024-09-06 17:30:08 -0300

    portfolio

--- public/js/slides.js
+++ public/js/slides.js
@@ -1,25 +1,25 @@
 let currentSlide = 0;
-const slides = document.querySelectorAll('.slide-atividades, .slide-sobre'); 
+const slides = document.querySelectorAll('.slide-atividades');
 
 function hideAllSlides() {
-  slides.forEach(slide => {
-    slide.classList.remove('active');
-  });
+    slides.forEach(slide => {
+        slide.classList.remove('active');
+    });
 }
 
 function showSlide(index) {
-  hideAllSlides();
-  slides[index].classList.add('active');
+    hideAllSlides();
+    slides[index].classList.add('active');
 }
 
 function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
 }
 
 function prevSlide() {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  showSlide(currentSlide);
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
 }
 
 document.querySelector('.next').addEventListener('click', nextSlide);
